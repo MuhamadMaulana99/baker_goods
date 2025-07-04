f332d3eb0720ba6c8826f7dc2189c7de38240bcd
muhamad maulana <maulanamuhamad593@gmail.com>
2025-07-04 14:52:56 +0700

    fix

---
+++ .next/static/chunks/app/layout-d00497f2c8e4b07a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{930:function(n,e,i){Promise.resolve().then(i.t.bind(i,47960,23)),Promise.resolve().then(i.bind(i,69064))},47960:function(){}},function(n){n.O(0,[587,64,971,117,744],function(){return n(n.s=930)}),_N_E=n.O()}]);
